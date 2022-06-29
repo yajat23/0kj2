844f9a1224cb167bc6e6da25fe1fe3cadc589254
yajat23 <89627077+yajat23@users.noreply.github.com>
2022-06-29 15:52:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+var previous_result = '';
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet', modelLoded);
+}
+
+function modelLoded() {
+  console.log('Model Loaded!');
+}
+
+function draw()
+{
+ image(video, 0, 0, 300, 300);
+ classifier.classify(video, gotResult);
+}
+
+function gotResult(error, results)
+{
+if(error){
+  console.error(error);
+} else
+{
+  if((results[0].confidence > 0.5) && (previous_results != results[0].label
+    )){
+      console.log(results);
+      previous_result = results[0].label;
+      var synth = window.speechSynthesis;
+      speak_data = 'object detected is - '+results[0].label;
+      var utterThis = new SpeechSynthesisUtterance(speak_data);
+      synth.speak(utterThis);
+
+      document.getElementById("result_object_name").innerHTML = results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+      
+    }
+  }
+}
